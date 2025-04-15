1960ead9553d4b6c93c9d99fe707ab764f3b8338
Luis T. A. Ramirez <89233817+luistaramirez@users.noreply.github.com>
2025-04-15 18:03:37 -0400

    Delete lang directory

--- lang/en/local_debtblocker.php
+++
@@ -1,5 +0,0 @@
-<?php
-// filepath: local/debtblocker/lang/en/local_debtblocker.php
-
-$string['pluginname'] = 'Debt Blocker';
-$string['debtblocker:description'] = 'Blocks or unblocks users based on their debt status retrieved from an external API.';
